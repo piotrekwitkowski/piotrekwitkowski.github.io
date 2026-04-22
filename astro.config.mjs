635d51edd75a56aa653800fe21cee454a9483be3
Piotrek <piotrekwitkowski@users.noreply.github.com>
2026-04-22 20:46:08 +0100

    Configure Vite to bundle Cloudscape for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ export default defineConfig({
   site: "https://piotrekwitkowski.github.io",
   output: "static",
   integrations: [react()],
+  vite: {
+    ssr: {
+      noExternal: ["@cloudscape-design/components", "@cloudscape-design/global-styles"],
+    },
+  },
 });
